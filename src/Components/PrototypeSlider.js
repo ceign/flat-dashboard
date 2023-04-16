4396e1d9548dd80565eb45bb4802a86b69c763a6
ceign <dnssdvlp@gmail.com>
2023-04-16 18:18:09 -0400

    Slider component prototype is done

--- src/Components/PrototypeSlider.js
+++ src/Components/PrototypeSlider.js
@@ -41,9 +41,42 @@ function PrototypeSlider(mainRef) {
 }
 
 export default function App() {
-  const [sliderRef, instanceRef] = useKeenSlider({
-    initial: 0,
-  });
+  const [sliderRef, instanceRef] = useKeenSlider(
+    {
+      initial: 0,
+      loop: true,
+    },
+    [
+      (slider) => {
+        let timeout;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 5000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      },
+    ]
+  );
   const [thumbnailRef] = useKeenSlider(
     {
       initial: 0,
